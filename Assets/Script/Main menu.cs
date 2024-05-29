69b076535c4a7cade821a6b781b939ac6f942174
Thichada Chotgodchaphat <txtshinesky0727@gmail.com>
2024-05-29 11:45:28 +0200

    Menu

--- Assets/Script/Main menu.cs
+++ Assets/Script/Main menu.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement; 
+using UnityEngine.SceneManagement;
+using TMPro; 
 
 public class Mainmanu : MonoBehaviour
 {
+    [SerializeField] private TMP_Text volumeTextValue = null;
     // Called when we click the "Play" button.
     public void OnPlayButton()
     {
@@ -15,5 +17,11 @@ public class Mainmanu : MonoBehaviour
     {
         Application.Quit();
     }
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        volumeTextValue.text = volume.ToString("0.0");
+    }
 
+    
 }
