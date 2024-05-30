26a3e89e848fdc2488c2a6928a67b89d53804ef6
Adonai <adonai.osterberg@elev.ga.lbs.se>
2024-05-30 13:02:38 +0200

    grog

--- Assets/Movement/Movement.cs
+++ Assets/Movement/Movement.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public FatassData data;
-    public StatApplier statApplier;
+    //public FatassData data;
+    //public StatApplier statApplier;
     public float moveSpeed = 5f;
     public Camera mainCamera; // Reference to the main camera
 
@@ -13,12 +13,12 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        moveSpeed = 5f + statApplier.speedMod;
+        //moveSpeed = 5f + statApplier.speedMod;
     }
 
     void Update()
     {
-        rb.drag = 0.1f + statApplier.weightMod;
+        //rb.drag = 0.1f + statApplier.weightMod;
         // Input handling for movement
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
